23db86b6c875c0dc307ef0024e80173889840e82
mohamed <mems_cool@hotmail.com>
2019-01-30 20:15:02 +0200

    updated
    Co-Authored-By: fatma abdelmoniem mohamed <fatmaabdelmoniem@users.noreply.github.com>
    Co-Authored-By: Kareem Salah <kareem1002009@users.noreply.github.com>
    Co-Authored-By: omarnasreldeen <omarnasreldeen@users.noreply.github.com>

--- Developement/model/post.php
+++ Developement/model/post.php
@@ -3,7 +3,7 @@ include_once('database.php');
 
 class post extends Database {
   public static function __construct($id) {
-      $sql = "SELECT * FROM posts WHERE id = $id;";
+      $sql = "SELECT * FROM post WHERE id = $id;";
       $statement = Database::$db->prepare($sql);
       $statement->execute();
       $data = $statement->fetch(PDO::FETCH_ASSOC);
@@ -11,26 +11,54 @@ class post extends Database {
       foreach ($data as $key => $value) {
         $this->{$key} = $value;
       }
-
-public static function create_Post($question,$programLanguage,$user_id)
+/////////3ayzen nshof date wla dates
+///tmm b2a $dates
+public static function create_post($question,$language,$user_id,$dates)
 {
-    $sql = "INSERT INTO posts (question,programLanguage,user_id) VALUES (?,?,?)";
-    Database::$db->prepare($sql)->execute([$question,$programLanguage,$user_id]);
+    $sql = "INSERT INTO post (question,language,user_id,dates) VALUES (?,?,?,?)";
+    Database::$db->prepare($sql)->execute([$question,$language,$user_id,$dates]);
 }
-public static function edit_post($question,$programLanguage,$user_id){
-    $sql = "UPDATE posts SET (question=$question,programLanguage=$programLanguage) WHERE id =$this->id ? AND user_id = $user_id;";
-			Database::$db->prepare($sql)->execute();
+public static function get_post($language,$dates,$question)
+{
+    $question = str_replace(" ", "%", $question);
+    $sql = "SELECT * FROM post WHERE language=$language OR dates=$dates OR
+    question like ('%$question%') ORDER BY dates";
+    $statement=Database::$db->prepare($sql)->execute([$question,$language,$user_id,$dates]);
+    $posts= [];
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $posts [] = $row;
+    }
 }
-public static function delete_post(){
-    $sql = "DELETE FROM posts WHERE id = $this->id;";
+
+
+public static function edit_post($question,$language,$user_id,$dates,$post_id){
+    $sql = "UPDATE post SET (question=$question,language=$language,dates=$dates)
+    WHERE id =$post_id  AND user_id = $user_id;";
+		Database::$db->prepare($sql)->execute();
+}
+public static function delete_post($post_id){
+    $sql = "DELETE FROM post WHERE id = $post_id;";
 			Database::$db->query($sql);
 }
 public static function bookmarked($user_id,$post_id){
   $sql="INSERT INTO bookmarked (user_id,post_id) VALUES (?,?) ;";
   Database::$db->prepare($sql)->execute([$user_id,$post_id]);
-
 }
-
-
+public static function remove_bookmarked($user_id,$post_id){
+  $sql="DELETE FROM bookmarked WHERE user_id=$user_id AND post_id=$post_id ;";
+  Database::$db->prepare($sql)->execute();
+}
+public static function search_post($keyword)
+{
+  $keyword = str_replace(" ", "%", $keyword);
+  $sql = "SELECT * FROM post WHERE question like ('%$keyword%') ORDER BY question;";
+  $statement = Database::$db->prepare($sql);
+  $statement->execute();
+  $posts = [];
+  while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $posts[] = $row;
+  }
+  return $posts;
+}
 }
 ?>
---
+++ Developement/model/visitor.php
@@ -0,0 +1,69 @@
+<?php
+include_once('database.php');
+class visitor extends Database{
+  public static function check_email ($email){ ////add to SRS
+    //Database::connect();
+        $sql = "SELECT email,password FROM user WHERE email = '$email';";
+        $statement = Database::$db->prepare($sql);
+        $statement->execute();
+      $row=$statement->fetch(PDO::FETCH_ASSOC);
+      if($email==$row['email'])
+      {
+      return 1;
+      }
+        return 0;
+      }
+
+  public static function sign_in ($email,$password){
+  //  Database::connect();
+        $sql = "SELECT email,password FROM user WHERE email = '$email';";
+        $statement = Database::$db->prepare($sql);
+        $statement->execute();
+      $row=$statement->fetch(PDO::FETCH_ASSOC);
+      if($email==$row['email'])
+      {
+        if ($password==$row['password'])
+            return 1;  //login success registered-user
+      }
+      else if {
+        $sql = "SELECT email,password FROM specialist WHERE email ='$email';"
+        $statement = Database::$db->prepare($sql);
+        $statement->execute();
+        $row=$statement->fetch(PDO::FETCH_ASSOC);
+        if($email==$row['email'])
+        {
+          if ($password==$row['password'])
+              return 2;  //login success - specialist
+        }
+      }
+      else return 0; // login failed
+    }
+public static function sign_up ($email,$password,$FirstName,$LastName){
+  //Database::connect();
+    $sql = "SELECT email FROM user WHERE email = $email;";
+    $statement = Database::$db->prepare($sql);
+    $statement->execute();
+  $row = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($email==$row['email'])
+    {
+        return 0; //signup error
+    }
+    $sql = "INSERT INTO user (first_name,last_name,email,password) VALUES (?,?,?,?)";
+    Database::$db->prepare($sql)->execute([$FirstName,$LastName,$email,$password]);
+    return 1; //signup success
+  }
+  public static function sign_up_specialist ($email,$password,$FirstName,$LastName,$bankinfo){
+    $sql = "SELECT email FROM specialist WHERE email = $email;";
+    $statement = Database::$db->prepare($sql);
+    $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC)
+    if ($email== $row['email'])
+    {
+        return 0; //signup error
+    }
+    $sql = "INSERT INTO specialist (first_name,last_name,email,password,bankinfo) VALUES (?,?,?,?,?)";
+    Database::$db->prepare($sql)->execute([$FirstName,$LastName,$email,$password,$bankinfo]);
+    return 1; //signup success
+  }
+}
+?>
