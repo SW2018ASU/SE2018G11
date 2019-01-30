1fa968baf5afae31e40de8e873130916197f6bc5
martinjowi <36242504+martinjowi@users.noreply.github.com>
2019-01-30 14:32:59 +0200

    $_SESSION["user_name"] = $user['first_name']

--- Codeguide/Controllers/login.php
+++ Codeguide/Controllers/login.php
@@ -1,21 +1,14 @@
 <?php
-
-include_once("../model/visitorclass.php");
-session_start();
-if( Visitor::sign_in($_POST['email'],$_POST['password'] )){
-
-
-$user=user::userid($_POST['email']);
-
-
-$_SESSION["user_id"] = $user['id'];
-// header('Location: ../homelogged.php');
-echo json_encode(['status'=>1]); //this will return a json with key status and value 1 to tell him it match 
-}
-else{
-
-echo json_encode(['status'=>0]);
-
+  include_once("../model/visitorclass.php");
+  session_start();
+  if( Visitor::sign_in($_POST['email'],$_POST['password'] )){
+  $user=user::userid($_POST['email']);
+  $_SESSION["user_id"] = $user['id'];
+  $_SESSION["user_name"] = $user['first_name'];
+  echo json_encode(['status'=>1]); //this will return a json with key status and value 1 to tell him it match
+  }
+  else{
+  echo json_encode(['status'=>0]);
 }
 
 
