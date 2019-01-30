ad9f387a2bea1def1a8efa1b82f8c793baf7c0ce
martinjowi <36242504+martinjowi@users.noreply.github.com>
2019-01-30 14:11:57 +0200

    Update createaccount.php
    $_SESSION["user_name"] = $user['first_name']  is added

--- Codeguide/Controllers/createaccount.php
+++ Codeguide/Controllers/createaccount.php
@@ -1,14 +1,14 @@
 <?php
-include_once("../model/visitorclass.php");
-session_start();
-if(Visitor::sign_up($_POST['email'],$_POST['password'],$_POST['firstname'],$_POST['lastname'])){
-$user=user::userid($_POST['email']);
-
-$_SESSION["user_id"] = $user['id'];
-header('Location: ../homelogged.php');
-}
-else{
-header('Location: ../signup.php');
+  session_start();
+  include_once("../model/visitorclass.php");
+  if(Visitor::sign_up($_POST['email'],$_POST['password'],$_POST['firstname'],$_POST['lastname'])){
+    $user=user::userid($_POST['email']);
+    $_SESSION["user_id"] = $user['id'];
+    $_SESSION["user_name"] = $user['first_name'];
+    header('Location: ../homelogged.php');
+  }
+  else{
+    header('Location: ../signup.php');
 }
 
 
