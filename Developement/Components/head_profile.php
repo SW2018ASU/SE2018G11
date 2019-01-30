9076f57732ba20d3d4ee6d7719bdf9df894f0e41
markyous <36309094+markyous@users.noreply.github.com>
2019-01-30 21:25:23 +0200

    Update head_profile.php
    Co-Authored-By: fatma abdelmoniem mohamed <fatmaabdelmoniem@users.noreply.github.com>
    Co-Authored-By: Kareem Salah <kareem1002009@users.noreply.github.com>
    Co-Authored-By: moerfan97 <moerfan97@users.noreply.github.com>
    Co-Authored-By: omarhesham1997 <omarhesham1997@users.noreply.github.com>

--- Developement/Components/head_profile.php
+++ Developement/Components/head_profile.php
@@ -107,7 +107,7 @@
 
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="img/profile.png" width="30 px"  height="30 px" alt=""><span> <?php echo $_SESSION["user_first_name"] ; ?> </span>
+                <img src="img/profile.png" width="30 px"  height="30 px" alt=""><span> <?php echo" ".ucwords($_SESSION["user_first_name"]); ?> </span>
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="./login.php">log out</a>
