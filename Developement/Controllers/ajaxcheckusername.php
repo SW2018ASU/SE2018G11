6e0488daf820cf3b4ed9ec2955727659ec4b57b3
markyous <36309094+markyous@users.noreply.github.com>
2019-01-30 19:43:19 +0200

    Update ajaxcheckusername.php

--- Developement/Controllers/ajaxcheckusername.php
+++ Developement/Controllers/ajaxcheckusername.php
@@ -1,7 +1,7 @@
 <?php
 include_once("../model/visitorclass.php");
 //this controller for ajax we check first that email exist in database or not
-if(Visitor::checkemail($_POST['email'])){//checkmail will return 1 if exist and 0 if not exist
+if(visitor::get_user_info($_POST['email'])){//checkmail will return 1 if exist and 0 if not exist
 
   echo "exist";//this will put in variable  data which is paramter in function (which mean this will be returned)
 
