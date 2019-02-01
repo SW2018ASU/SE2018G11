8ec97965f4b6b63a358d226875df14de60a2e395
omar hesham <omarnasr3939@gmail.com>
2019-02-01 10:39:18 +0200

    update

--- Developement/Components/head_homelogged.php
+++ Developement/Components/head_homelogged.php
@@ -52,7 +52,7 @@
   <body>
     <header>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
-        <img class="mr-2" src="img/logo.png" width="40 px"  height="35 px"/>
+        <a href="home.php"><img class="mr-2" src="img/logo.png" width="40 px"  height="35 px"/></a>  
         <a class="navbar-brand" href="#"><span style="color:#5f6bdd;">C</span>ode<span style="color:#5f6bdd;">G</span>uide</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
