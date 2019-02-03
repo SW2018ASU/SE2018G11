760610616e3543e8e097e6c0c1ff040c18d66668
omar hesham <omarnasr3939@gmail.com>
2019-02-03 18:18:06 +0200

    Update answers.php
    Co-Authored-By: Kareem Salah <kareem1002009@users.noreply.github.com>
    Co-Authored-By: moerfan97 <moerfan97@users.noreply.github.com>
    Co-Authored-By: mario465 <mario465@users.noreply.github.com>
    Co-Authored-By: omarnasreldeen <omarnasreldeen@users.noreply.github.com>

--- Developement/answers.php
+++ Developement/answers.php
@@ -16,12 +16,69 @@ $posts=post::get_post_by_id($post_id);
           ?>
           <div class="card mb-3">
             <div class="card-header">
-              <?php echo $post['language'] ?>
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="img/profile.png" width="30 px">
+              <?php echo $post['user_name'] ?>
             </div>
+            <div class="col-lg-4">
+              <img src="img/calender.png" width="20 px">
+              <?php echo $post['dates']?>
+            </div>
+            <div class="col-lg-4">
+              <img src="img/time.png" width="20 px">
+              <?php echo $post['times']?>
+            </div>
+          </div>
+          </div>
             <div class="card-body">
-              <h5 class="card-title">
-                <?php echo $post['user_name'] ?>
-              </h5>
+              <h5 class="card-title">  <?php
+              if ($post["language"]=="C/C++") {
+                ?>
+                  <img src="img/c++.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="C#") {
+                ?>
+                  <img src="img/csharp.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="java") {
+                ?>
+                  <img src="img/java.png" height="30px"alt="">
+                <?php
+              }
+              else if ($post["language"]=="python") {
+                ?>
+                  <img src="img/python.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="php") {
+                ?>
+                  <img src="img/php.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="CSS") {
+                ?>
+                  <img src="img/css.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="HTML") {
+                ?>
+                  <img src="img/html.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="javascript") {
+                ?>
+                  <img src="img/javascript.png" height="30px" alt="">
+                <?php
+              }
+              else if ($post["language"]=="jquery") {
+                ?>
+                  <img src="img/jquery.png" height="30px" alt="">
+                <?php
+              }
+  echo $post["language"];?></h5>
               <p class="card-text">
                 <?php echo $post['question'] ?>
               </p>
