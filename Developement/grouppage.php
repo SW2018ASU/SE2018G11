1102e17dff6edf78a8e8362c9835696c15f66264
martinjowi <36242504+martinjowi@users.noreply.github.com>
2019-02-04 20:29:09 +0200

    Update grouppage.php

--- Developement/grouppage.php
+++ Developement/grouppage.php
@@ -85,23 +85,119 @@
               <p class="card-text" style="border:solid 1px #5f6bdd">
               <?php echo $post["question"];?></p>
               <hr>
-              <div class="row rowC">
+              <div class="row rowC<?php echo $post['post_id'] ?>">
                 <div class="col-lg-4">
-                  <button type="button" class="btn btn-light btn-lg btn-block"><img src="img/answer.png" width="20px">  Answers</button>
+                  <button type="button" id="answer_<?php echo $post["post_id"] ?>" class="btn btn-light btn-lg btn-block"><img src="img/answer.png" width="20px">  Answers</button>
                 </div>
                 <div class="col-lg-4">
-                  <button type="button" class="btn btn-light btn-lg btn-block comment"  ><img src="img/comment.png" width="20px">  Comment</button>
+                  <button type="button" class="btn btn-light btn-lg btn-block comment" id='comment_<?php echo $post['post_id'] ?>' ><img src="img/comment.png" width="20px">  Comment</button>
                 </div>
                 <div class="col-lg-4">
                   <button type="button" class="btn btn-light btn-lg btn-block"><img src="img/bookmark2.png" width="20px">  Bookmark</button>
                 </div>
               </div>
               <!-- comments forms -->
-              <form class="formC" action="index.html" method="post">
-                <div class="divC"></div>
+              <form class="formC<?php echo $post['post_id'] ?>" action="profile.php" method="post">
+                <div class="divC<?php echo $post['post_id'] ?>">
+                  <hr class='lead'><div class='input-group my-1'><div class='input-group-prepend'><span class='input-group-text'>Your comment</span></div><textarea class='form-control' id='comment_text_<?php echo $post['post_id'] ?>' aria-label='With textarea'></textarea></div>
+                  <div class="d-flex flex-row-reverse bd-highlight">
+                    <button class='btn btn-light' id='comment_button_<?php echo $post['post_id'] ?>' type='button' ><img src='img/send.png' ></button>
+                  </div>
+                </div>
               </form>
             </div>
+            <!-- //begining of comment -->
+            <div class="divA<?php echo $post['post_id'] ?>" style="display:none"; >
+              <?php
+              $comments=comment::show_comment($post['post_id']);
+
+              foreach ($comments as $comment) {
+                if($comment['post_id']==$post['post_id'])
+                {
+               ?>
+              <!-- loop for comments -->
+              <!-- These are the answers -->
+              <div class="card mb-3 ml-5">
+                <!-- username appears here -->
+                <div class="card-header">
+                  <div class="row">
+                  <!-- username -->
+                  <div class="col-lg-4">
+                    <img src="img/profile.png" width="30 px">
+                  <?php echo $comment['user_name'] ?>
+                </div>
+                <div class="col-lg-4">
+                  <img src="img/calender.png" width="20 px">
+                  <?php echo $comment['dates'] ?>
+                </div>
+                <div class="col-lg-4">
+                  <img src="img/time.png" width="20 px">
+                  <?php echo $comment['times'] ?>
+                </div>
+                </div>
+                </div>
+                <div class="card-body">
+                  <!-- text goes here -->
+                  <p class="card-text"><?php echo $comment['comment_text'] ?></p>
+                  <hr>
+                  <div class="row">
+                    <div class="col-lg-4">
+                    </div>
+                    <div class="col-lg-4">
+                    </div>
+                    <div class="col-lg-4">
+                      <button type="button" class="btn btn-light btn-lg btn-block"><img src="img/helpful.png" width="20px">  Helpful</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- end of answer -->
+              <!-- end of loop -->
+            <?php }} ?>
+              <!-- //end of comment -->
+            </div>
+
           </div>
+          <script type="text/javascript">
+          $(".divC<?php echo $post['post_id'] ?>").hide();
+            $(document).ready(function(){
+          $("#answer_<?php echo $post['post_id'] ?>").click(function(){
+            if($(".divA<?php echo $post['post_id'] ?>").is(":visible")){
+              $(".divA<?php echo $post['post_id'] ?>").hide();
+            }
+            else  {
+              $(".divA<?php echo $post['post_id'] ?>").show();
+            }
+          }
+          );
+          $("#comment_<?php echo $post['post_id'] ?>").click(function(){
+          if($(".divC<?php echo $post['post_id'] ?>").is(":visible")){
+            $(".divC<?php echo $post['post_id'] ?>").hide();
+          }
+          else  {
+            $(".divC<?php echo $post['post_id'] ?>").show();
+          }
+          });
+          $("#comment_button_<?php echo $post['post_id'] ?>").click(function(){
+          var formData = {
+          'comment_text'        : $('#comment_text_<?php echo $post['post_id'] ?>').val(),
+          'post_id'             : <?php echo $post['post_id'] ?>
+          };
+          $.ajax({
+          type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+          url         : 'Controllers/create_comment.php', // the url where we want to POST//where controller that we want to go to is exist
+          data        : formData, // our data object //this data will be sent to contrller in $_POST
+          dataType    : 'json', // what type of data do we expect back from the server
+          encode          : true
+          }).done(function(data) {
+            $(".divC<?php echo $post['post_id'] ?>").hide();
+            $(".divA<?php echo $post['post_id'] ?>").show();
+            $('#comment_text_<?php echo $post['post_id'] ?>').val("");
+            $(".divA<?php echo $post['post_id'] ?>").prepend("<div class='card mb-3 ml-5'><div class='card-header'><div class='row'><div class='col-lg-4'> <img src='img/profile.png' width='30 px'>  "+data['user_name']+"   </div>                <div class='col-lg-4'>    <img src='img/calender.png' width='20 px'> "+ data['dates'] +"   </div>    <div class='col-lg-4'> <img src='img/time.png' width='20 px'>     " +data['times']+"        </div> </div>     </div>     <div class='card-body'>   <p class='card-text'>"+data['comment_text']+"</p><hr><div class='row'><div class='col-lg-4'></div><div class='col-lg-4'></div><div class='col-lg-4'><button type='button' class='btn btn-light btn-lg btn-block'><img src='img/helpful.png' width='20px'>  Helpful</button> </div>  </div>   </div>");
+            });
+          });
+          });
+          </script>
           <?php } ?>
           <!-- loop end -->
         </div>
@@ -163,7 +259,6 @@
           </div>
         </div>
       </div>
-    </div>
     </div>
   </body>
 </html>
