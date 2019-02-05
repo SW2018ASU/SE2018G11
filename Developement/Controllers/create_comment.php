9a49d67437cfc46a186190a6ba1a694f33e123c4
martinjowi <36242504+martinjowi@users.noreply.github.com>
2019-02-05 16:26:39 +0200

    Update create_comment.php

--- Developement/Controllers/create_comment.php
+++ Developement/Controllers/create_comment.php
@@ -4,6 +4,7 @@ Database::connect();
 session_start();
 date_default_timezone_set("Africa/Cairo");
 comment::create_comment($_POST['comment_text'],$_SESSION["user_id"],$_POST['post_id'],date("h:ia"),date("Y/m/d"));
+$number=comment::get_number_comments($_POST['post_id']);
 $getcomment=comment::show_comment($_POST['post_id']);
-echo json_encode(['user_name'=>$getcomment[0]['user_name'],'dates'=>$getcomment[0]['dates'],'times'=>$getcomment[0]['times'],'comment_text'=>$getcomment[0]['comment_text'],]);
+echo json_encode(['user_name'=>$getcomment[0]['user_name'],'dates'=>$getcomment[0]['dates'],'times'=>$getcomment[0]['times'],'comment_text'=>$getcomment[0]['comment_text'],'comments_number'=>$number['number']]);
 ?>
